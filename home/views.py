ab2819f4c4b2beca4b9a34dc1e0131468e800edd
Fernando Galvan <n.brighter00@gmail.com>
2022-09-14 11:37:22 -0500

    adding variables and settings

--- home/views.py
+++ home/views.py
@@ -1,12 +1,15 @@
+from json import load
 from django.shortcuts import render
 import requests
+import os
+
 
 # Create your views here.
 def search(request):
     query = request.GET.get('q')
 
     if query:
-        request.get("https://api.themoviedb.org/3/search/movie?api_key=<<api_key>>&language=en-US&page=1&include_adult=false")
+        request.get("https://api.themoviedb.org/3/search/movie?api_key={os.getenv('APIKEY')}&language=en-US&page=1&include_adult=false")
 
 
     return render(request, 'search.html')
