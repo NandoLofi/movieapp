821b3f7dc02b7b6a3336bb477cb7053a17271d31
Fernando Galvan <n.brighter00@gmail.com>
2022-09-14 11:16:58 -0500

    setting up basics db

--- home/views.py
+++ home/views.py
@@ -1,3 +1,12 @@
 from django.shortcuts import render
+import requests
 
 # Create your views here.
+def search(request):
+    query = request.GET.get('q')
+
+    if query:
+        request.get("https://api.themoviedb.org/3/search/movie?api_key=<<api_key>>&language=en-US&page=1&include_adult=false")
+
+
+    return render(request, 'search.html')
